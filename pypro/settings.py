97bf27f0bf990f2b2d78f13dcca5efda409a7e47
javanurbieta <olajava11@gmail.com>
2023-03-29 20:34:56 -0300

    Comando de Coleta de Arquivos Estáticos

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os.path
 from functools import partial
 from pathlib import Path
 from typing import Any
@@ -123,4 +124,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
